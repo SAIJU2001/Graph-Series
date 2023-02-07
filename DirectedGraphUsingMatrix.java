c12fd43342ae9f0264dd01e5250d3dd7e5bf9aca
saikat naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-02-07 15:56:49 +0530

    Add files via upload

---
+++ DirectedGraphUsingMatrix.java
@@ -0,0 +1,41 @@
+//Introduction to graph
+//create a directed graph using matrix
+
+import java.util.*;
+
+public class DirectedGraphUsingMatrix
+{
+    public static void main( String args[] )
+    {
+        Scanner Sc=new Scanner(System.in);
+
+        //taking the row & col val for matrix
+        System.out.print("Enter the row : ");
+        int row=Sc.nextInt();
+
+        System.out.print("Enter the col : ");
+        int col=Sc.nextInt();
+
+        //create the matrix
+        int graph[][]=new int[row+1][col+1];
+
+        //total no of edge or simply node connection
+        System.out.print("Enter the no of connection : ");
+        int connect=Sc.nextInt();
+
+        for( int i=0 ; i<connect ; i++ )
+        {
+            System.out.print("Enter the Index no : ");
+            int x=Sc.nextInt();
+            int y=Sc.nextInt();
+
+            //directed
+            graph[x][y]=1; //we assume the edge weight is 1 
+        }
+
+        //print all the edge
+        for( int i=0 ; i<graph.length ; i++ )
+            for( int j=0 ; j<graph[0].length ; j++ )
+                System.out.print( graph[i][j]+" ");
+    }
+}
