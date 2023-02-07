f741f556d92a28f60de1adc52d9dda62fd9a2c72
saikat naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-02-07 15:58:45 +0530

    Add files via upload

---
+++ UndirectedGraphUsingList.java
@@ -0,0 +1,45 @@
+//Introduction to graph
+//create a undirected graph using ArrayList
+
+import java.util.*;
+
+public class UndirectedGraphUsingList
+{
+    public static void main( String args[] )
+    {
+        Scanner Sc=new Scanner(System.in);
+
+        //taking the row & col val for matrix
+        System.out.print("Enter the no of node : ");
+        int countNode=Sc.nextInt();
+
+        ArrayList<ArrayList<Integer>>graph=new ArrayList<ArrayList<Integer>>();
+
+        //row formation in the list
+        for( int i=0 ; i<=countNode ; i++ )
+            graph.add( new ArrayList<Integer>() );
+
+        //total no of edge or simply node connection
+        System.out.print("Enter the no of connection : ");
+        int connect=Sc.nextInt();
+
+        for( int i=0 ; i<connect ; i++ )
+        {
+            System.out.print("Enter the Index no : ");
+            int x=Sc.nextInt();
+            int y=Sc.nextInt();
+
+            graph.get(x).add(y); //we assume the edge weight is 1
+            graph.get(y).add(x); 
+        }
+
+        //print all the edge
+        for( int i=0 ; i<=countNode ; i++ )
+        {
+            for( int j=0 ; j<graph.get(i).size() ; j++ )
+                System.out.print( graph.get(i).get(j)+" ");
+
+            System.out.println();
+        }
+    }
+}
